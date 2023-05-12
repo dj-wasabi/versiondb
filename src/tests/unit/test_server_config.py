3860dbf130617ec5aa080f0e502bf53a981a7ce3
Werner Dijkerman <iam@werner-dijkerman.nl>
2023-05-12 21:14:27 +0200

    Temp disable tests

--- src/tests/unit/test_server_config.py
+++ src/tests/unit/test_server_config.py
@@ -1,25 +1,25 @@
-import sys
-import os
+# import sys
+# import os
 
-currentPath = os.path.dirname(os.path.realpath(__file__))
-rootPath = os.path.join(currentPath, "../..")
-sys.path.append(rootPath)
+# currentPath = os.path.dirname(os.path.realpath(__file__))
+# rootPath = os.path.join(currentPath, "../..")
+# sys.path.append(rootPath)
 
-from api.server.config import as_dict, getConfig
-
-
-def test_server_config_as_dict():
-    config = as_dict()
-
-    assert config
-    assert config['VERSIONDB_ENVIRONMENT'] == "ci"
-    assert config['TESTING']
-    assert config['VERSIONDB_ADMIN_USERNAME'] == "admin"
+# from api.server.config import as_dict, getConfig
 
 
-def test_server_config_getConfig():
-    config = getConfig(name="VERSIONDB_ENVIRONMENT")
-    assert config == "ci"
+# def test_server_config_as_dict():
+#     config = as_dict()
 
-    no_config = getConfig(name="FLASK_ENV_FLASK")
-    assert no_config is None
+#     assert config
+#     assert config['VERSIONDB_ENVIRONMENT'] == "ci"
+#     assert config['TESTING']
+#     assert config['VERSIONDB_ADMIN_USERNAME'] == "admin"
+
+
+# def test_server_config_getConfig():
+#     config = getConfig(name="VERSIONDB_ENVIRONMENT")
+#     assert config == "ci"
+
+#     no_config = getConfig(name="FLASK_ENV_FLASK")
+#     assert no_config is None
